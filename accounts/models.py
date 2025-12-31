4185aedf9ad66007d880b22d11c0eed9508eb2e9
Артём Ткалин <t-los@yandex.ru>
2025-12-31 17:14:05 +0300

    simple user add

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """Простая модель пользователя для магазина"""
+    
+    # Добавляем только самое необходимое
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.email or self.username
--- digimart/settings.py
+++ digimart/settings.py
@@ -87,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
 
-
+AUTH_USER_MODEL = 'accounts.User'
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
